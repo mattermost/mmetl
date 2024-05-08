25a54139a3b8cf0b09a6716fca5c9248c372e70b
Colton Shaw <46071821+coltoneshaw@users.noreply.github.com>
2024-05-08 13:00:09 -0400

    Adjusted user mention parsing, added tests, and parsing for attachment fallback. (#44)
    * Adjusted user mentions and added a test
    
    * Adjusted tests and added attachment fallback

--- services/slack/parse.go
+++ services/slack/parse.go
@@ -211,7 +211,7 @@ func (t *Transformer) SlackConvertUserMentions(users []SlackUser, posts map[stri
 	}
 
 	// Special cases.
-	regexes["@here"], _ = regexp.Compile(`<!here\|@here>`)
+	regexes["@here"], _ = regexp.Compile("<(!|@)here>")
 	regexes["@channel"], _ = regexp.Compile("<!channel>")
 	regexes["@all"], _ = regexp.Compile("<!everyone>")
 
@@ -223,6 +223,12 @@ func (t *Transformer) SlackConvertUserMentions(users []SlackUser, posts map[stri
 			for mention, r := range regexes {
 				post.Text = r.ReplaceAllString(post.Text, mention)
 				posts[channelName][postIdx] = post
+
+				if post.Attachments != nil {
+					for _, attachment := range post.Attachments {
+						attachment.Fallback = r.ReplaceAllString(attachment.Fallback, mention)
+					}
+				}
 			}
 		}
 	}
@@ -250,6 +256,12 @@ func (t *Transformer) SlackConvertChannelMentions(channels []SlackChannel, posts
 			for channelReplace, r := range regexes {
 				post.Text = r.ReplaceAllString(post.Text, channelReplace)
 				posts[channelName][postIdx] = post
+
+				if post.Attachments != nil {
+					for _, attachment := range post.Attachments {
+						attachment.Fallback = r.ReplaceAllString(attachment.Fallback, channelReplace)
+					}
+				}
 			}
 		}
 	}
